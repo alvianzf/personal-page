e492c670755d776f965a08fd5008d1ee742197a5
alvianzf <alvianzf@gmail.com>
2024-12-16 08:47:21 +0100

    deat: add tooltip for accessibility

--- src/components/Hero/SocialLink.tsx
+++ src/components/Hero/SocialLink.tsx
@@ -19,7 +19,7 @@ const SocialLink: React.FC<SocialLinkProps> = ({ href, icon, label }) => {
     >
       {icon}
 
-      <span className="absolute left-1/2 bottom-full mb-2 w-max -translate-x-1/2 rounded text-gray-800 bg-white text-xs py-1 px-2 opacity-0 group-hover:opacity-100 transition-opacity pointer-events-none">
+      <span className="absolute left-1/2 bottom-full mb-2 w-max -translate-x-1/2 rounded bg-white text-gray-800 text-xs py-1 px-2 opacity-0 group-hover:opacity-100 transition-opacity pointer-events-none shadow-lg">
         {label}
       </span>
     </a>
