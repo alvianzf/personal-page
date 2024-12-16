8fa1c11396215766865bf8a6b2477cf08c832890
alvianzf <alvianzf@gmail.com>
2024-12-16 08:11:34 +0100

    feat: add relevant experiences: git push

--- src/components/About/ExperienceCard.tsx
+++ src/components/About/ExperienceCard.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React from "react";
 
 interface ExperienceCardProps {
   company: string;
+  location?: string | null;
   role: string;
   period: string;
   description: string;
@@ -10,17 +11,21 @@ interface ExperienceCardProps {
 
 const ExperienceCard: React.FC<ExperienceCardProps> = ({
   company,
+  location,
   role,
   period,
   description,
-  achievements
+  achievements,
 }) => {
   return (
     <div className="bg-gray-800 rounded-lg p-6 hover:shadow-lg transition-shadow">
-      <h4 className="text-xl font-semibold text-blue-400">{company}</h4>
+      <div className=" flex justify-between">
+        <h4 className="text-xl font-semibold text-blue-400">{company}</h4>
+        <p className="text-sm text-gray-400">{period}</p>
+      </div>
       <div className="flex justify-between items-center mb-4">
         <p className="text-lg text-gray-300">{role}</p>
-        <p className="text-sm text-gray-400">{period}</p>
+        <p>{location}</p>
       </div>
       <p className="text-gray-300 mb-4">{description}</p>
       <ul className="list-disc list-inside space-y-2">
